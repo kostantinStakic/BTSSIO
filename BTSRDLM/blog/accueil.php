579dd19cd8b2a21f24a48f4ebaee028bc4dafdbb
kostantinStakic <kostantin.stakic@mediaschool.education>
2025-05-16 11:54:13 +0200

    up

--- BTSRDLM/blog/accueil.php
+++ BTSRDLM/blog/accueil.php
@@ -1,7 +1,8 @@
 <?php
+include("bdd.php");
 //connexion à la bdd avec l'extension mysqli()
 // la classe mysqli(hostname,user,password,dbname)
-$bdd = new mysqli("localhost","root","","blog");
+$bdd = new mysqli($hostname,$user,$password,$dbname);
 
 ?>
 <!DOCTYPE html>
@@ -12,11 +13,25 @@ $bdd = new mysqli("localhost","root","","blog");
 	<title>BLOG</title>
 </head>
 <body>
+	<form method="get">
+		<input type="text" name="mail">
+		<input type="text" name="mdp">
+		<input type="reset" value="ANNULER">
+		<input type="submit" name="login" value="LOGIN">
+	</form>
+
 	<table>
 		<tr>
-			<td><img src="images/riff.jpeg"></td>
-			<td></td>
+			<td><img src="images/riff.jpeg" width="400"></td>
+			<td style="vertical-align: top;">
+				<span style="font-weight: 900;">
+				Titre
+				</span>
+				<p>Publication, bla bla bla ....
+				</p>
+			</td>
 		</tr>
 	</table>
+
 </body>
 </html>
---
+++ BTSRDLM/blog/bdd.php
@@ -0,0 +1,9 @@
+<?php
+// variables qui vont contenir les 
+// données pour faciliter l'accès à la connexion bdd
+$hostname = "localhost";
+$user = "root";
+$password = "";
+$dbname = "blog";
+
+?>
